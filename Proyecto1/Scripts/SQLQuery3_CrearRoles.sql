288adf40b14e89f098c1323f71fd442f4e7e358e
David15Barrera <david1315barrera@gmail.com>
2024-06-20 11:51:33 -0600

    fix bug in PR4

--- Proyecto1/Scripts/SQLQuery3_CrearRoles.sql
+++ Proyecto1/Scripts/SQLQuery3_CrearRoles.sql
@@ -11,6 +11,18 @@ BEGIN
     DECLARE @DescripcionLog NVARCHAR(100);
     DECLARE @RoleId uniqueidentifier = NEWID(); -- Generar nuevo Id único
 
+	   -- Validar que el nombre del rol no esté vacío
+    IF LEN(LTRIM(RTRIM(@RoleName))) = 0
+    BEGIN
+        SET @DescripcionLog = 'Error: El nombre del rol no puede estar vacío';
+        INSERT INTO proyecto1.HistoryLog ([Date], Description)
+        VALUES (GETDATE(), @DescripcionLog);
+        RETURN;
+    END
+
+	--Cambiamos el formato de la sentencia 
+    SET @RoleName = UPPER(LEFT(@RoleName, 1)) + LOWER(SUBSTRING(@RoleName, 2, LEN(@RoleName) - 1));
+
     -- Verificar si el rol ya existe en la tabla Roles
     IF NOT EXISTS (SELECT * FROM proyecto1.Roles WHERE RoleName = @RoleName)
     BEGIN
@@ -26,7 +38,7 @@ BEGIN
     ELSE
     BEGIN
         -- Registro en HistoryLog si el rol ya existe
-        SET @DescripcionLog = 'El rol ' + @RoleName + ' ya existe en la tabla Roles';
+        SET @DescripcionLog = 'Error: El rol ' + @RoleName + ' ya existe en la tabla Roles';
         INSERT INTO proyecto1.HistoryLog ([Date], Description)
         VALUES (GETDATE(), @DescripcionLog);
     END
@@ -35,11 +47,11 @@ GO
 
 -- Para hacer pruebas;
 -- Creamos un nuevo rol
-EXEC proyecto1.PR4 @RoleName = 'NuevoRol';
+--EXEC proyecto1.PR4 @RoleName = 'NuevoRol';
 
 --verificamos si existe
-SELECT * FROM proyecto1.Roles;
+--SELECT * FROM proyecto1.Roles;
 
 --Verificamos si ya se ingreso en el historia
 
-SELECT * FROM proyecto1.HistoryLog;
+--SELECT * FROM proyecto1.HistoryLog;
