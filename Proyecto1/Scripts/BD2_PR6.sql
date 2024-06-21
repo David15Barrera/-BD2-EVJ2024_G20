c7f783b7c218ee67094001ab7b5d74d201675713
David15Barrera <david1315barrera@gmail.com>
2024-06-21 15:12:57 -0600

    Update regex

--- Proyecto1/Scripts/BD2_PR6.sql
+++ Proyecto1/Scripts/BD2_PR6.sql
@@ -11,7 +11,8 @@ CREATE PROCEDURE proyecto1.PR6
 AS
 BEGIN
     SET NOCOUNT ON;
-	-- Validaciones de Usuario
+
+    -- Validaciones de Usuario
     IF @EntityName = 'Usuarios'
     BEGIN
         IF ISNULL(@FirstName, '') NOT LIKE '%[^a-zA-Z ]%' AND ISNULL(@LastName, '') NOT LIKE '%[^a-zA-Z ]%'
@@ -22,8 +23,20 @@ BEGIN
     -- Validacion de Curso
     ELSE IF @EntityName = 'Course'
     BEGIN
-        IF ISNULL(@Name, '') NOT LIKE '%[^[a-zA-Z]+( \d+)? ]%' AND ISNUMERIC(@CreditsRequired) = 1
-            SET @IsValid = 1;
+        -- Validación del nombre del curso
+        IF @Name IS NOT NULL
+        BEGIN
+            IF @Name LIKE '[a-zA-Z]%' AND @Name NOT LIKE '%[^a-zA-Z0-9 ]%'
+                SET @IsValid = 1;
+            ELSE
+                SET @IsValid = 0;
+        END
+        ELSE
+            SET @IsValid = 0;
+
+        -- Validación de créditos requeridos
+        IF @CreditsRequired IS NOT NULL AND @CreditsRequired > 0
+            SET @IsValid = @IsValid & 1;
         ELSE
             SET @IsValid = 0;
     END
@@ -33,4 +46,5 @@ BEGIN
         SET @IsValid = 0;
     END;
 END;
+
 GO
